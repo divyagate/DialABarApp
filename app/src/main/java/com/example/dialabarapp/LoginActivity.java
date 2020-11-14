88354192b8964436446c55763792c7ce7770d81a
divya <gatedivya123@gmail.com>
2020-11-14 18:57:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dialabarapp/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.dialabarapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+}
